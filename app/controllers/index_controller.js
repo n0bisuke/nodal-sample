1df0e697cc1b82bb31b4f152c700086bd6e0e294
n0bisuke <rsksound@gmail.com>
2016-02-13 21:52:25 +0900

    first commit

---
+++ app/controllers/index_controller.js
@@ -0,0 +1,26 @@
+module.exports = (function() {
+
+  'use strict';
+
+  const Nodal = require('nodal');
+
+  class IndexController extends Nodal.Controller {
+
+    get() {
+      this.render(
+        Nodal.Template.generate('layout.html', 'index.html').render(
+          this.params,
+          {
+            test: this.params.query.test,
+            name: 'myapp Application'
+          }
+        )
+      );
+
+    }
+
+  }
+
+  return IndexController;
+
+})();
